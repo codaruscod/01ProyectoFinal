24dea2258feed700690213c3c78aa8145144b33f
codaruscod <andrvigr@gmail.com>
2024-05-30 21:15:43 -0500

    subscripciones finalizadas

--- src/routes/personas.routes.js
+++ src/routes/personas.routes.js
@@ -38,7 +38,95 @@ router.post('/add', upload.single('file') , async (req, res) => {
         res.status(500).json({ message: error.message });
     }
 });
+/* --------------------------- Crear subscripcion --------------------------- */
+router.get('/addsub', (req, res) => {
+    res.render('personas/addsub')
+});
 
+router.post('/addsub', upload.single('file') , async (req, res) => {
+    try {
+        const { sub, description, price} = req.body
+        let newsub = {}
+        if(req.file){
+            const file = req.file
+            const imagen_original = file.originalname
+            const imagen = file.filename
+            newsub = { sub, description, price}
+        }else{
+            newsub = {sub, description, price}
+        }
+        await pool.query('INSERT INTO subs SET ?', [newsub]);
+        res.redirect('/listsub');
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+/* ------------------------------------ x ----------------------------------- */
+/* ------------------------------------ listar subscripcion ----------------------------------- */
+router.get('/listsub', async (req, res) => {
+    try {
+        const [result] = await pool.query('SELECT * FROM subs');
+        res.render('personas/listsub', { sub: result })
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
+router.get('/delete/:id', async (req, res) => {
+    try {
+        const { id } = req.params
+        await pool.query('DELETE FROM subs WHERE id = ?', [id]);
+        res.redirect('/listsub');
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
+/* --------------------------- borrar y editarsubscripcion -------------------------- */
+// Ruta para eliminar una suscripción
+router.get('/deletesub/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        await pool.query('DELETE FROM subs WHERE id = ?', [id]);
+        res.redirect('/listsub');
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
+// Ruta para mostrar el formulario de edición de una suscripción
+router.get('/editsub/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const [sub] = await pool.query('SELECT * FROM subs WHERE id = ?', [id]);
+        const subEdit = sub[0];
+        res.render('personas/editsub', { sub: subEdit });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
+// Ruta para procesar la edición de una suscripción
+router.post('/editsub/:id', upload.single('file'), async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { sub, description, price } = req.body;
+        let editsub = {};
+        if (req.file) {
+            const file = req.file;
+            const imagen_original = file.originalname;
+            const imagen = file.filename;
+            editsub = { sub, description, price };
+        } else {
+            editsub = { sub, description, price };
+        }
+        await pool.query('UPDATE subs SET ? WHERE id = ?', [editsub, id]);
+        res.redirect('/listsub');
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+/* ------------------------------------ x ----------------------------------- */
 router.get('/list', async (req, res) => {
     try {
         const [result] = await pool.query('SELECT * FROM personas');
